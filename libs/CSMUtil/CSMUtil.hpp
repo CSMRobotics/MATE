67dcaa73ea3ad9653a33282e07cd7c0aa8e04b58
PhocaeaUnitas <90007371+PhocaeaUnitas@users.noreply.github.com>
2021-09-22 10:29:23 -0600

    Added CSMUtil utility library

---
+++ libs/CSMUtil/CSMUtil.hpp
@@ -0,0 +1,10 @@
+
+class CSMUtil{
+    public:
+        CSMUtil() = default;
+        ~CSMUtil() = default;
+
+        static int imap(float value, float from_min, float from_max, int to_min, int to_max){
+            return (int)((value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min);
+        }
+};
