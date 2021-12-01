61f29a63d24efbc4f1fdc7d07437cf6199ad8043
Zac Stanton <zac71113@gmail.com>
2021-11-30 20:43:33 -0700

    Quaternion testing (mostly) done. Should work just fine for our purposes

--- ROV.cpp
+++ ROV.cpp
@@ -20,25 +20,6 @@ void init() {
     // server->start();
 }
 
-void test() {
-    csmutil::Quaternionf q(1,2,3,4);
-    csmutil::Quaternionf p(4,3,2,1);
-
-    std::cout << q << std::endl;
-    std::cout << p << std::endl;
-    csmutil::Quaternionf result(q*p);
-
-    std::cout << result << std::endl;
-    std::cout << result.getNorm() << std::endl;
-    std::cout << result.getAsUnit() << std::endl;
-    std::cout << result.getConjugate() << std::endl;
-    std::cout << result.getReciprocal() << std::endl;
-    std::pair<csmutil::Vector3f, float> axisangle = result.getAxisAngle();
-    std::cout << (axisangle.first) << ' ' << axisangle.second << std::endl;
-
-    exit(0);
-}
-
 int main() {
     // initialize ROV parts
     test();
