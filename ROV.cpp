0674621a8e24e31814d9f31d21184b34e99e2a79
Zac Stanton <zac71113@gmail.com>
2021-11-30 20:46:04 -0700

    Update ROV.cpp

--- ROV.cpp
+++ ROV.cpp
@@ -5,34 +5,32 @@
 #include "Joystick.hpp"
 #include "ServoDriver.hpp"
 
-#include "CSMUtil.hpp"
-
-Component* components[1];
-// TCP_Client* client = new TCP_Client();
-//TCP_Server* server = new TCP_Server();
-// ServoDriver* servoDriver = new ServoDriver();
+Component* components[2];
+TCP_Client* client = new TCP_Client();
+TCP_Server* server = new TCP_Server();
+ServoDriver* servoDriver = new ServoDriver();
+Joystick js = Joystick(server); // create network joystick
 // Joystick js = Joystick("/dev/input/by-id/usb-Logitech_Extreme_3D_pro_00000000002A-joystick");
 
 void init() {
-    // components[0] = new Drive();
-    // components[0] = new Manipulator(&js, servoDriver);
-    // client->start();
-    // server->start();
+    components[0] = new Drive();
+    components[1] = new Manipulator(&js, servoDriver);
+    client->start();
+    server->start();
 }
 
 int main() {
     // initialize ROV parts
-    test();
     init();
     
     // start update loop
     bool quit = false;
-    // while(!quit) {
-    //     js.updatePresses();
-    //     for(Component* component : components) {
-    //         component->Update();
-    //     }
-    // }
+    while(!quit) {
+        js.updatePresses();
+        for(Component* component : components) {
+            component->Update();
+        }
+    }
     
     return 0;
 }
