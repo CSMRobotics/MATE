e0116eaeb5ec965f5fdfc570895792da09837c2b
Yamea <zac71113@gmail.com>
2022-01-26 18:28:20 -0700

    setup BAR02 library for implementation

--- libs/CSMUtil/BAR02.cpp
+++ libs/CSMUtil/BAR02.cpp
@@ -1,1 +1,44 @@
 #include "BAR02.hpp"
+
+#include <cmath>
+extern "C" {
+    #include <linux/i2c-dev.h>
+    #include <i2c/smbus.h>
+}
+
+csmutil::MS5837::MS5837() {
+    // create fd to i2c file
+    // initialize any member variables that need to be
+}
+
+void csmutil::MS5837::Update() {
+
+}
+
+void csmutil::MS5837::AutoUpdate() {
+    Update();
+}
+
+void csmutil::MS5837::Stop() {
+    return;
+}
+
+float pressure(float conversion=1.0f) {
+
+}
+
+float temperature() {
+
+}
+
+float depth() {
+
+}
+
+float altitude() {
+
+}
+
+void calculate() {
+
+}
