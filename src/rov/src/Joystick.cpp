43bcf1be6d789755f7cfc357f1af13585d552603
yameatmeyourdead <zac71113@gmail.com>
2022-02-26 16:25:22 -0700

    Begin transition to ROS2
    Co-authored-by: Lucas Niewohner <lucasniewohner@gmail.com>

--- src/rov/src/Joystick.cpp
+++ src/rov/src/Joystick.cpp
@@ -1,32 +1,31 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/joy.hpp"
+#include "std_msgs/msg/string.hpp"
 using std::placeholders::_1;
 
-class JoystickSubscriber : public rclcpp::Node
+class Joystick : public rclcpp::Node
 {
   public:
-    JoystickSubscriber()
+    Joystick()
     : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
-      "topic", 10, std::bind(&JoystickSubscriber::topic_callback, this, _1));
+      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&Joystick::topic_callback, this, _1));
     }
 
   private:
-    void topic_callback(const sensor_msgs::msg::Joy::SharedPtr msg) const
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
     {
-        
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
     }
-
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<JoystickSubscriber>());
+  rclcpp::spin(std::make_shared<Joystick>());
   rclcpp::shutdown();
   return 0;
 }
