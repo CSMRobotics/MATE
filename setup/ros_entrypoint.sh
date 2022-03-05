ee007d259c59ed4e4a16d3538d31197bcf246edf
Lucas Niewohner <lucas@birdseyerobotics.com>
2022-03-05 14:41:06 -0700

    Fix the basestation build and refactor. Add joystick.

---
+++ setup/ros_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+export CSM_WS=/opt/ros/CSM_MATE_WS
+
+ros_env_setup="$CSM_WS/install/setup.bash"
+echo "sourcing   $ros_env_setup"
+source "$ros_env_setup"
+
+echo "ROS_ROOT   $ROS_ROOT"
+echo "ROS_DISTRO $ROS_DISTRO"
+
+exec "$@"
