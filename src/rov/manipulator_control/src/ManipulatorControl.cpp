3effc2f84bbd3af02ad72c261ad02cf711e60116
yameatmeyourdead <zac71113@gmail.com>
2022-03-12 15:30:27 -0700

    Create Manipulator Control and associated messages

--- src/rov/manipulator_control/src/ManipulatorControl.cpp
+++ src/rov/manipulator_control/src/ManipulatorControl.cpp
@@ -1,10 +1,28 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include "rov_interfaces/msg/setpoints.hpp"
+#include "rov_interfaces/msg/pwm.hpp"
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+using std::placeholders::_1;
 
-  printf("hello world manipulator_control package\n");
-  return 0;
+class ManipulatorControl : public rclcpp::Node {
+public:
+    ManipulatorControl() : Node(std::string("manipulator_control")) {
+        _subscription = this->create_subscription<rov_interfaces::msg::Setpoints>(
+            "manipulator_setpoints", 10, std::bind(&ManipulatorControl::topic_callback, this, _1));
+        _publisher = this->create_publisher<rov_interfaces::msg::PWM>("PWM", 10);
+    }
+
+private:
+    void topic_callback(const rov_interfaces::msg::Setpoints::SharedPtr msg) const {
+        // do stuff with setpoints
+    }
+    rclcpp::Publisher<rov_interfaces::msg::PWM>::SharedPtr _publisher;
+    rclcpp::Subscription<rov_interfaces::msg::Setpoints>::SharedPtr _subscription;
+};
+
+int main(int argc, char ** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<ManipulatorControl>());
+    rclcpp::shutdown();
+    return 0;
 }
