861fad2426ba9f0494190731fb98f5d8a2c0bcbf
yameatmeyourdead <zac71113@gmail.com>
2022-03-12 18:21:15 -0700

    implement PCA9685 Node (should be working)

---
+++ src/rov/pca9685/src/PCA9685_Node.cpp
@@ -0,0 +1,56 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include "rov_interfaces/msg/pwm.hpp"
+#include "rov_interfaces/srv/create_continuous_servo.hpp"
+#include "rov_interfaces/srv/create_servo.hpp"
+
+#include "JHPWMPCA9685.h"
+#include "ServoDriver.hpp"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+class PCA9685_Node : public rclcpp::Node {
+public:
+    PCA9685_Node() : Node(std::string("pca9685")) {
+        _pwm_subscription = this->create_subscription<rov_interfaces::msg::PWM>(std::string("pwm"), 10, 
+            std::bind(&PCA9685_Node::topic_callback, this, _1));
+        _create_continuous_servo_service = this->create_service<rov_interfaces::srv::CreateContinuousServo>("create_continuous_servo", 
+            std::bind(&PCA9685_Node::create_continuous_servo, this, _1, _2));
+        _create_servo_service = this->create_service<rov_interfaces::srv::CreateServo>("create_servo", 
+            std::bind(&PCA9685_Node::create_servo, this, _1, _2));
+    }
+
+private:
+    void topic_callback(const rov_interfaces::msg::PWM::SharedPtr msg) {
+        if(msg->is_continuous_servo) {
+            servoDriver.setThrottle(msg->channel, msg->angle_or_throttle);
+        } else {
+            servoDriver.setAngle(msg->channel, msg->angle_or_throttle);
+        }
+    }
+
+    void create_continuous_servo(const std::shared_ptr<rov_interfaces::srv::CreateContinuousServo_Request> request, 
+            std::shared_ptr<rov_interfaces::srv::CreateContinuousServo_Response> response) {
+        servoDriver.addContinuousServo(request->channel);
+        response->result = true;
+    }
+
+    void create_servo(const std::shared_ptr<rov_interfaces::srv::CreateServo_Request> request, 
+            std::shared_ptr<rov_interfaces::srv::CreateServo_Response> response) {
+        servoDriver.addServo(request->channel);
+        response->result = true;
+    }
+
+    ServoDriver servoDriver;
+    rclcpp::Subscription<rov_interfaces::msg::PWM>::SharedPtr _pwm_subscription;
+    rclcpp::Service<rov_interfaces::srv::CreateContinuousServo>::SharedPtr _create_continuous_servo_service;
+    rclcpp::Service<rov_interfaces::srv::CreateServo>::SharedPtr _create_servo_service;
+};
+
+int main(int argc, char ** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<PCA9685_Node>());
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ src/rov/pca9685/src/ServoDriver.cpp
@@ -0,0 +1,138 @@
+#include "ServoDriver.hpp"
+#include <iostream>
+#include <cassert>
+
+static int f2imap(float value, float from_min, float from_max, int to_min, int to_max){
+    return ((value - from_min) * ((to_max - to_min) / (from_max - from_min)) + to_min);
+}
+
+ServoDriver::ServoDriver(){
+    this->driver_board = PCA9685();
+    assert(("Failed to open PCA9685", this->driver_board.openPCA9685()));
+    this->driver_board.setAllPWM(0,0);
+    this->driver_board.reset();
+    this->setPWMFrequency(this->driver_board_frequency);
+}
+
+ServoDriver::~ServoDriver(){}
+
+void ServoDriver::addServo(int channel){
+    if(this->isChannelNotInUse(channel)){
+        Servo new_servo;
+        new_servo.initialized = true;
+        new_servo.type = ServoType::POSITIONAL;
+        new_servo.setpoint_minimum = 0.0f;
+        new_servo.setpoint_maximum = 180.0f;
+        this->servos[channel] = new_servo;
+        this->setPWMBounds(channel, 500, 2500);
+    }
+}
+
+void ServoDriver::addContinuousServo(int channel){
+    if(this->isChannelNotInUse(channel)){
+        Servo new_servo;
+        new_servo.initialized = true;
+        new_servo.type = ServoType::CONTINUOUS;
+        new_servo.setpoint_minimum = -1.0f;
+        new_servo.setpoint_maximum = 1.0f;
+        this->servos[channel] = new_servo;
+        this->setPWMBounds(channel, 500, 2500);
+    }
+}
+
+void ServoDriver::removeServo(int channel){
+    if(this->isServoChannelInUse(channel)){
+        this->servos[channel].initialized = false;
+    }
+}
+
+void ServoDriver::removeContinuousServo(int channel){
+    if(this->isContinuousServoChannelInUse(channel)){
+        this->servos[channel].initialized = false;
+    }
+}
+
+void ServoDriver::setAngle(int channel, float angle){
+    if(this->isServoChannelInUse(channel)){
+        Servo* working_servo = &this->servos[channel];
+	assert(("Servo angle" + std::to_string(angle) + " is not in range [" + std::to_string(working_servo->setpoint_minimum) + ", " + std::to_string(working_servo->setpoint_maximum) + "]", angle >= working_servo->setpoint_minimum && angle <= working_servo->setpoint_maximum));
+        working_servo->setpoint = angle;
+        this->setPWM(channel, f2imap(working_servo->setpoint, working_servo->setpoint_minimum, working_servo->setpoint_maximum, working_servo->pwm_minimum, working_servo->pwm_maximum));
+    }
+}
+
+void ServoDriver::setThrottle(int channel, float throttle){
+    if(this->isContinuousServoChannelInUse(channel)){
+        Servo* working_servo = &this->servos[channel];
+	assert(("Servo angle " + std::to_string(throttle) + "is not in range [" + std::to_string(working_servo->setpoint_minimum) + ", " + std::to_string(working_servo->setpoint_maximum) + "]", throttle >= working_servo->setpoint_minimum && throttle <= working_servo->setpoint_maximum));
+        working_servo->setpoint = throttle;
+        this->setPWM(channel, f2imap(working_servo->setpoint, working_servo->setpoint_minimum, working_servo->setpoint_maximum, working_servo->pwm_minimum, working_servo->pwm_maximum));
+	
+    }
+}
+
+void ServoDriver::setAngleBounds(int channel, float minimum_angle, float maximum_angle){
+    if(this->isServoChannelInUse(channel)){
+        Servo* working_servo = &this->servos[channel];
+        working_servo->setpoint_minimum = minimum_angle;
+        working_servo->setpoint_maximum = maximum_angle;
+    }
+}
+
+void ServoDriver::setThrottleBounds(int channel, float minimum_throttle, float maximum_throttle){
+    if(this->isContinuousServoChannelInUse(channel)){
+        Servo* working_servo = &this->servos[channel];
+        working_servo->setpoint_minimum = minimum_throttle;
+        working_servo->setpoint_maximum = maximum_throttle;
+    }
+}
+
+void ServoDriver::setPWMBounds(int channel, int minimum_us, int maximum_us){
+    if(this->isChannelInUse(channel)){
+        Servo* working_servo = &this->servos[channel];
+        float counts_per_microsecond = this->getCountsPerMicrosecond();
+	working_servo->pwm_minimum = minimum_us * counts_per_microsecond;
+        working_servo->pwm_maximum = maximum_us * counts_per_microsecond;
+    }
+}
+
+void ServoDriver::setPWM(int channel, int value){
+    if(this->isChannelInUse(channel)){
+        this->driver_board.setPWM(channel, 0, value);
+    }
+}
+
+void ServoDriver::setPWMFrequency(float frequency){
+    this->driver_board.setPWMFrequency(frequency);
+}
+
+bool ServoDriver::isChannelInUse(int channel){
+    assert(("Channel number " + std::to_string(channel) + " is not in range [0, 15]", channel >= 0 && channel <= 15 ));
+    return this->servos[channel].initialized;
+}
+
+bool ServoDriver::isChannelNotInUse(int channel){
+    return !isChannelInUse(channel);
+};
+
+bool ServoDriver::isServoChannelInUse(int channel){
+    if(this->isChannelInUse(channel)) {
+        assert(("Channel number " + std::to_string(channel) + " is not a Servo", this->servos[channel].type == ServoType::POSITIONAL));
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool ServoDriver::isContinuousServoChannelInUse(int channel){
+    if(this->isChannelInUse(channel)) {
+        assert(("Channel number " + std::to_string(channel) + " is not a Continuous Servo", this->servos[channel].type == ServoType::CONTINUOUS));
+        return true;
+    } else {
+        return false;
+    }
+}
+
+float ServoDriver::getCountsPerMicrosecond(){
+    return (this->driver_board_frequency * 4096) / 1000000;
+}
