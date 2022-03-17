616e3453bca13f8d7cee7f1e9945fccb64cf8431
yameatmeyourdead <zac71113@gmail.com>
2022-03-16 19:12:47 -0600

    fix capitalization

--- src/rov/flight_controller/setup.py
+++ src/rov/flight_controller/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'flightcontroller = flight_controller.flightcontroller:main'
+            'flightcontroller = flight_controller.FlightController:main'
         ],
     },
 )
