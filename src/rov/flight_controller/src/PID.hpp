1e2a1cd1bfe8e3038042af5f356743cbcedf646d
yameatmeyourdead <zac71113@gmail.com>
2022-03-28 12:32:32 -0600

    linear PID should work

--- src/rov/flight_controller/src/PID.hpp
+++ src/rov/flight_controller/src/PID.hpp
@@ -6,40 +6,54 @@
 
 class LinearPID {
 public:
-    LinearPID(float kp = 0.0, float ki = 0.0, float kd = 0.0);
+    LinearPID(float kp = 0.0, float ki = 0.0, float kd = 0.0, float kfb = 1.0);
 
     // Update the controller optional argument dt_us for artificial time
     float Update(long long dt_us = NULL);
 
     // Set the accumulated integral value. Units are in error*time.
-    void setIntegral(float integralValue);
+    void setIntegral(float integralValue) {this->integral = integralValue;};
     
     // Invert the direction of the recieved output for PID controllers
     // Producing positive feedback loops instead of negative ones
-    void invertFeedback(bool invert);
+    void invertFeedback(bool invert) {this->doInvertFeedback = invert;};
 
     // Current output from the PID controller
-    float getOutput();
+    float getOutput() {return this->output;};
 
-    void setSetpoint(float setpoint);
+    void setSetpoint(float setpoint) {this->setpoint = setpoint;};
 
-    void setFeedbackFunction(std::function<float(void)> func);
+    void setFeedbackFunction(std::function<float(void)> func) {this->feedback = func;};
+
+    void setBias(float bias) {this->bias = bias;};
 private:
+    // PID gain constant
     float kp, ki, kd;
-    float kfb = 1.0;
+    // feedback gain constant
+    float kfb;
+
+    long long dt_us;
 
     float setpoint;
+    float error;
+    float lastError;
     float integral;
-    float output;
-    float lastOutput;
+    float output = 0;
+    float lastOutput = output;
+    float bias = 0.0;
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> initTime;
+    std::chrono::time_point<std::chrono::high_resolution_clock> timelast;
 
     bool doInvertFeedback = false;
 
-    std::function<float(void)> feedback;
+    std::function<float(void)> feedback = std::bind(&LinearPID::defaultFeedback, this);
     float calcError();
     float calcProportional();
     float calcIntegral();
     float calcDerivative();
+
+    float defaultFeedback() {return 0.0;};
 };
 
 class NonLinearPI {
