ea1921ccf204b6300383388eda215d195fb9c3f0
yameatmeyourdead <zac71113@gmail.com>
2023-02-17 15:26:39 -0700

    implement ServoDriver functionality

--- src/rov/pca9685/src/main_interactive.cpp
+++ src/rov/pca9685/src/main_interactive.cpp
@@ -1,12 +1,69 @@
 #include <iostream>
+#include <map>
 
 #include <rclcpp/rclcpp.hpp>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 
+#include "pca9685/ServoDriver.hpp"
+
 int main(int argc, char** argv) {
     std::string userInput;
+    std::map<int, bool> isContinuous;
+
+    ServoDriver servoDriver;
+
+    std::cout << "For all 16 pins, C for continuous N for non continuous\n";
+    for(int i = 0; i < 16; i++) {
+        std::cout << "Pin " << i << ": ";
+        std::getline(std::cin, userInput);
+        std::cout << '\n';
+        boost::algorithm::to_lower(userInput);
+        if(userInput.at(0) == 'c') {
+            servoDriver.addContinuousServo(i);
+            isContinuous[i] = true;
+        } else {
+            // default to non continuous
+            servoDriver.addServo(i);
+            isContinuous[i] = false;
+        }
+    }
+
+    std::cout << "\nFor all 16 pins, enter PWM range separated by spaces (newline for default 500-2500)\n";
+    for(int i = 0; i < 16; i++) {
+        std::cout << "Pin " << i << ": ";
+        std::getline(std::cin, userInput);
+        boost::algorithm::to_lower(userInput);
+        int idx = userInput.find(' ');
+        int lower_bound = 500;
+        int upper_bound = 2500;
+        if(idx != 0 && idx != std::string::npos) {
+            lower_bound = boost::lexical_cast<int>(userInput.substr(0, idx));
+            upper_bound = boost::lexical_cast<int>(userInput.substr(idx+1));
+        }
+        servoDriver.setPWMBounds(i, lower_bound, upper_bound);
+    }
+
+    std::cout << "\nFor all 16 pins, set boundaries for Angle/Throttle\n";
+    for(int i = 0; i < 16; i++) {
+        std::cout << "Pin " << i << ": ";
+        std::getline(std::cin, userInput);
+        std::cout << '\n';
+        boost::algorithm::to_lower(userInput);
+        int lower_bound = 0;
+        int upper_bound = 0;
+        int idx = userInput.find(' ');
+        if(idx != 0 && idx != std::string::npos) {
+            lower_bound = boost::lexical_cast<int>(userInput.substr(0, idx));
+            upper_bound = boost::lexical_cast<int>(userInput.substr(idx+1));
+        }
+        if(isContinuous.at(i)) {
+            servoDriver.setThrottleBounds(i, lower_bound, upper_bound);
+        } else {
+            servoDriver.setAngleBounds(i, lower_bound, upper_bound);
+        }
+    }
 
     while(true) {
         // get user input
