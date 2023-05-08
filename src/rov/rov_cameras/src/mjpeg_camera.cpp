2a1a8c8ae6f5a703a6de5e1e619600ce3a11dadb
yameatmeyourdead <zac71113@gmail.com>
2023-05-07 23:25:01 -0500

    Fix cv::Mat size and cvColor flag

--- src/rov/rov_cameras/src/mjpeg_camera.cpp
+++ src/rov/rov_cameras/src/mjpeg_camera.cpp
@@ -9,12 +9,16 @@ MJPEG_Camera::MJPEG_Camera(std::string device, uint8_t pub_id) : rclcpp::Node(st
     image_pub = this->create_publisher<sensor_msgs::msg::CompressedImage>(std::string("camera_mjpeg") + std::to_string(static_cast<int>(pub_id)) + std::string("/image_raw/compressed"), 10);
     poll_func = this->create_wall_timer(std::chrono::milliseconds(1000/30), std::bind(&MJPEG_Camera::poll, this));
 
-    cv::Mat yComponent(240, 320, CV_8UC1);
-    cv::Mat uComponent(240/2, 320/2, CV_8UC1);
-    cv::Mat vComponent(240/2, 320/2, CV_8UC1);
-    std::vector<cv::Mat> yuvComponents = {yComponent, uComponent, vComponent};
+    // cv::Mat yComponent(240, 320, CV_8UC1);
+    // cv::Mat uComponent(240/2, 320/2, CV_8UC1);
+    // cv::Mat vComponent(240/2, 320/2, CV_8UC1);
 
-    cv::merge(yuvComponents, img_i420);
+    // cv::resize(uComponent, uComponent, cv::Size(320, 240));
+    // cv::resize(vComponent, vComponent, cv::Size(320, 240));
+
+    // std::vector<cv::Mat> yuvComponents = {yComponent, uComponent, vComponent};
+
+    // cv::merge(yuvComponents, img_i420);
 
     char pipeline[1025] = {0};
     snprintf(pipeline, sizeof(pipeline)-1, PIPELINE_F, device.c_str());
@@ -24,7 +28,7 @@ MJPEG_Camera::MJPEG_Camera(std::string device, uint8_t pub_id) : rclcpp::Node(st
 void MJPEG_Camera::poll() {
     cap.grab();
     cap.retrieve(this->img_i420);
-    cv::cvtColor(this->img_i420, this->img_bgr, cv::COLOR_YUV2BGR_NV12);
+    cv::cvtColor(this->img_i420, this->img_bgr, cv::COLOR_YUV2BGR_I420, 3);
     sensor_msgs::msg::CompressedImage msg;
     std_msgs::msg::Header header;
     header.set__stamp(this->now());
