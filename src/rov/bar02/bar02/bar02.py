3257a8afbaa6385dd0b0e198faeb7e0d85251846
yameatmeyourdead <zac71113@gmail.com>
2023-05-12 11:15:51 -0600

    fix bar02 depends fix bar02 data type

--- src/rov/bar02/bar02/bar02.py
+++ src/rov/bar02/bar02/bar02.py
@@ -1,17 +1,17 @@
 from time import sleep
-import ms5837
+from . import ms5837
 
 import rclpy
 from rclpy.node import Node
 
-from rov_interfaces.msg import bar02_data
+from rov_interfaces.msg import Bar02Data
 
 class Bar02_Node(Node):
     OSR = ms5837.OSR_1024
 
     def __init__(self):
         super().__init__("bar02")
-        self._publisher = self.create_publisher(bar02_data, "bar02", 10)
+        self._publisher = self.create_publisher(Bar02Data, "bar02", 10)
         self.sensor = ms5837.MS5837_02BA()
         self.sensor.setFluidDensity(992.72) # rough estimate of density of pool water (we arent going to use sat liquid tables)
         self.sensor.init()
@@ -20,7 +20,7 @@ class Bar02_Node(Node):
 
     def timer_callback(self):
         self.sensor.read(self.OSR)
-        msg = bar02_data()
+        msg = Bar02Data()
         msg.altitude = self.sensor.altitude()
         msg.depth = self.sensor.depth()
         msg.pressure = self.sensor.pressure() # mbar
