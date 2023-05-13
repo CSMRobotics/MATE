6cc90cab8ed72e81da31b9a3ca4838b5edccd81c
yameatmeyourdead <zac71113@gmail.com>
2023-05-12 21:06:30 -0600

    Fix libboost build to actually build all of boost builds rqt_image_view and all of its dependencies

--- src/rov/run_docker.sh
+++ src/rov/run_docker.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-#xhost +
+xhost +
 docker run --runtime nvidia -it --rm -v /bin/systemctl:/bin/systemctl -v /run/systemd/system:/run/systemd/system -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket -v /sys:/sys:ro -v /usr/lib/gstreamer-1.0/:/usr/lib/gstreamer-1.0 --net=host --pid=host -v /dev:/dev -v /run/udev:/run/udev:ro --privileged --gpus all -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /usr/lib/aarch64-linux-gnu/gstreamer-1.0/libgstnvcompositor.so:/usr/lib/aarch64-linux-gnu/gstreamer-1.0/libgstnvcompositor.so -e DISPLAY=$DISPLAY -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 csmmaterobotics/competition:latest
 # docker run -it --rm --runtime nvidia -v /tmp/.X11-unix/:/tmp/.X11-unix csmmaterobotics/rov:latest
