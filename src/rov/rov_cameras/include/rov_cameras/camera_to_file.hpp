9471997b76be95a066bb7b299c8a748c8424bae8
yameatmeyourdead <zac71113@gmail.com>
2023-05-13 13:21:50 -0600

    stop stepping on stuff

--- src/rov/rov_cameras/include/rov_cameras/camera_to_file.hpp
+++ src/rov/rov_cameras/include/rov_cameras/camera_to_file.hpp
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include <thread>
 
-#define PIPELINE_H264_F "gst-launch-1.0 v4l2src device=%s ! nvv4l2decoder ! nvvidconv ! mp4mux ! filesink location=%s"
-#define PIPELINE_MJPEG_F "gst-launch-1.0 v4l2src device=%s io-mode=2 ! image/jpeg,width=320,height=240,framerate=30/1 ! jpegparse ! jpegdec ! jpegenc ! avimux ! filesink location=%s"
+#define PIPELINE_H264_F_STREAM "gst-launch-1.0 v4l2src device=%s ! nvv4l2decoder ! nvvidconv ! mp4mux ! filesink location=%s"
+#define PIPELINE_MJPEG_F_STREAM "gst-launch-1.0 v4l2src device=%s io-mode=2 ! image/jpeg,width=320,height=240,framerate=30/1 ! jpegparse ! jpegdec ! jpegenc ! avimux ! filesink location=%s"
 
 class File_Camera : public rclcpp::Node {
 public:
--- src/rov/rov_cameras/src/camera_to_file.cpp
+++ src/rov/rov_cameras/src/camera_to_file.cpp
@@ -4,9 +4,9 @@ File_Camera::File_Camera(const std::string& device, uint8_t id, const std::strin
     char command[1025] = {0};
 
     if(mjpeg)
-        snprintf(command, sizeof(command)-1, PIPELINE_MJPEG_F, device.c_str(), file_name.c_str());
+        snprintf(command, sizeof(command)-1, PIPELINE_MJPEG_F_STREAM, device.c_str(), file_name.c_str());
     else
-        snprintf(command, sizeof(command)-1, PIPELINE_H264_F, device.c_str(), file_name.c_str());
+        snprintf(command, sizeof(command)-1, PIPELINE_H264_F_STREAM, device.c_str(), file_name.c_str());
 
 
     auto pipeline = [&](){
