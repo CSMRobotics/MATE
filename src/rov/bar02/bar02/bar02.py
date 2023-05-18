0ff8646b2e684fbfd3f68bd8264118d7f6e7f339
Zac Stanton <zac71113@gmail.com>
2023-05-17 22:02:02 -0600

    use correct timing with OSR, pressure now in kPa

--- src/rov/bar02/bar02/bar02.py
+++ src/rov/bar02/bar02/bar02.py
@@ -7,8 +7,6 @@ from rclpy.node import Node
 from rov_interfaces.msg import Bar02Data
 
 class Bar02_Node(Node):
-    OSR = ms5837.OSR_1024
-
     def __init__(self):
         super().__init__("bar02")
         self._publisher = self.create_publisher(Bar02Data, "bar02", 10)
@@ -16,22 +14,22 @@ class Bar02_Node(Node):
         self.sensor.setFluidDensity(992.72) # rough estimate of density of pool water (we arent going to use sat liquid tables)
         self.sensor.init()
         sleep(0.01)
-        self.timer = self.create_timer(0.01, self.timer_callback)
+        # data sheet says 17.2 ms max time for OSR 8192 (roughly 60 hz)
+        self.timer = self.create_timer(18/1000, self.timer_callback)
 
     def timer_callback(self):
-        self.sensor.read(self.OSR)
+        self.sensor.read()
         msg = Bar02Data()
         msg.altitude = self.sensor.altitude()
         msg.depth = self.sensor.depth()
-        msg.pressure = self.sensor.pressure() # mbar
+        msg.pressure = self.sensor.pressure(conversion=ms5837.UNITS_kPa) # kPa
         msg.temperature_c = self.sensor.temperature() # deg C
         self._publisher.publish(msg)
 
 def main():
     rclpy.init()
 
-    bar02 = Bar02_Node()
-    rclpy.spin(bar02)
+    rclpy.spin(Bar02_Node())
 
     rclpy.shutdown()
 
