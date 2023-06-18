b909cde4498b1a2b2ff452dda505cfaff6453ff9
Zac Stanton <zac71113@gmail.com>
2023-06-18 15:13:16 -0600

    Refactor manipulator (#24)
    * init refactor (functionality not included)
    
    * v1 of manipulator controller implemented
    
    * velocity based > positional based

---
+++ src/rov/manipulator_controller/include/manipulator_controller/manipulator_controller.hpp
@@ -0,0 +1,22 @@
+#ifndef MANIPULATOR_CONTROLLER_HPP
+#define MANIPULATOR_CONTROLLER_HPP
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "rov_interfaces/msg/pwm.hpp"
+#include "rov_interfaces/msg/manipulator_setpoints.hpp"
+
+#include <unordered_map>
+
+class ManipulatorController : public rclcpp::Node {
+public:
+    ManipulatorController();
+
+private:
+    void setpoint_callback(const rov_interfaces::msg::ManipulatorSetpoints::SharedPtr msg);
+
+    rclcpp::Publisher<rov_interfaces::msg::PWM>::SharedPtr pwm_pub;
+    rclcpp::Subscription<rov_interfaces::msg::ManipulatorSetpoints>::SharedPtr manip_setpoints;
+};
+
+#endif
---
+++ src/rov/manipulator_controller/src/manipulator_controller.cpp
@@ -0,0 +1,52 @@
+#include "manipulator_controller/manipulator_controller.hpp"
+#include <algorithm>
+
+namespace {
+    void declare_manipulator_controller_parameters(rclcpp::Node*const node) {
+        node->declare_parameter("wrist_invert", false);
+        node->declare_parameter("wrist_lower_bound", 0.0);
+        node->declare_parameter("wrist_upper_bound", 180.0);
+        node->declare_parameter("wrist_pwm_pin", 14);
+        node->declare_parameter("clamp_invert", false);
+        node->declare_parameter("clamp_lower_bound", 0.0);
+        node->declare_parameter("clamp_upper_bound", 180.0);
+        node->declare_parameter("clamp_pwm_pin", 11);
+    }
+
+    void clamp(float* toClamp, double lower, double upper) {
+        *toClamp = std::min(*toClamp, static_cast<float>(upper));
+        *toClamp = std::max(*toClamp, static_cast<float>(lower));
+    }
+}
+
+ManipulatorController::ManipulatorController() : Node(std::string("manipulator_controller")) {
+    declare_manipulator_controller_parameters(this);
+
+    manip_setpoints = this->create_subscription<rov_interfaces::msg::ManipulatorSetpoints>("manipulator_setpoints", 10, std::bind(&ManipulatorController::setpoint_callback, this, std::placeholders::_1));
+    pwm_pub = this->create_publisher<rov_interfaces::msg::PWM>("PWM", 10);
+}
+
+void ManipulatorController::setpoint_callback(const rov_interfaces::msg::ManipulatorSetpoints::SharedPtr msg) {
+    static float wrist_pos_last = 0;
+    static float clamp_pos_last = 0;
+    // map values from rov_control
+    float wrist = wrist_pos_last += msg->wrist * ((this->get_parameter("wrist_invert").as_bool() << 1) - 1);
+    float clamp = clamp_pos_last += msg->clamp * ((this->get_parameter("clamp_invert").as_bool() << 1) - 1);
+
+    // clamp the received values
+    ::clamp(&wrist, this->get_parameter("wrist_lower_bound").as_double(), this->get_parameter("wrist_upper_bound").as_double());
+    ::clamp(&clamp, this->get_parameter("clamp_lower_bound").as_double(), this->get_parameter("clamp_upper_bound").as_double());
+
+    // publish PWM
+    // wrist pwm
+    auto pwm = rov_interfaces::msg::PWM();
+    pwm.angle_or_throttle = wrist;
+    pwm.channel = this->get_parameter("wrist_pwm_pin").as_int();
+    pwm_pub->publish(pwm);
+
+    // clamp pwm
+    pwm = rov_interfaces::msg::PWM();
+    pwm.angle_or_throttle = clamp;
+    pwm.channel = this->get_parameter("clamp_pwm_pin").as_int();
+    pwm_pub->publish(pwm);
+}
