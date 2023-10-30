3226f4c4f3dcabd55570da0da56a0c6dd7dd6c0b
yameatmeyourdead <zac71113@gmail.com>
2023-10-30 23:05:59 +0000

    finish migrating docker images to 20.04 humble

---
+++ setup/ros_entrypoint_common.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+export _WS=/opt/ros/humble
+
+ros_env_setup="$_WS/install/setup.bash"
+echo "sourcing   $ros_env_setup"
+source "$ros_env_setup"
+
+echo "ROS_ROOT   $ROS_ROOT"
+echo "ROS_DISTRO $ROS_DISTRO"
+
+exec "$@"
