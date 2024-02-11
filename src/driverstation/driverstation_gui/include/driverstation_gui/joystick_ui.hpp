0d9885153cb2cdf7bea4bf7534d169fcf1c44a40
PhocaeaUnitas <90007371+PhocaeaUnitas@users.noreply.github.com>
2024-02-11 14:14:22 -0700

    (Finally) Merge Raylib Driverstation UI (and Humble Migration) (#30)
    The commit talked about in legends. It has finally come upon us to ditch flask and merge the thing that cam had written for like 1.5 years now.
    
    * Added current driverstation progress
    
    See: https://github.com/Colorado-School-of-Mines-Robotics-Club/MATE_ROV_Programming/issues/8
    See: https://github.com/Colorado-School-of-Mines-Robotics-Club/MATE_ROV_Programming/issues/17
    
    * Removed build scripts;
    Added `.clangd` to .gitignore;
    
    * Removed build scripts;
    Added `.clangd` to .gitignore;
    
    * remove .clangd from git tracking
    
    * change directory structure to match ROS standard
    update cmakelists to match ROS standard
    massage some warnings out of the code
    
    * version check raylib (and by extension raygui)
    correctly install the include directory to install location
    
    * add todos, fix some compiler warnings
    
    * Fixed all asset issues
    
    * add depends for image transport
    fix some merge conflicts
    
    * Added model assets (were ignored by outer `.gitignore` as compilation artifacts);
    Replaced hex character literals with hex numeric literals;
    Corrected variable types on vector-iterating for loops;
    Removed unused variables;
    Removed now-ignored `.clangd` from index;
    Flagged unused parameters as `[[maybe_unused]]`;
    Flagged intentional fallthroughs with `[[fallthrough]]`;
    
    * `DriverstationUi::setCameraImage` can be used to set image data of individual cameras;
    `DriverstationUi::registerEstopCallback` can be used to add a callback function to be called when the estop is triggered;
    
    * missing #endif in header
    
    * extraneous ; in header file
    
    * fix -Wwrite-strings by adding const modifier
    
    * Driverstation now accepts `CompressedImage` messages;
    
    * Prevent colcon from building submodules
    add raygui and raylib as submodules
    
    * create commonv8_4, humble common dockerfile. yet to be built
    
    * finish migrating docker images to 20.04 humble
    
    * properly add submodules
    
    * Updated submodules to raylib v5.0 and raygui v4.0;
    Updated driverstation to work with new raylib and raygui versions;
    Fixed issue where orientation viewport model path was not being adjusted to the ROS directory;
    
    * Update ros2_humble_build_and_test.yaml from bs_launch to driverstation_gui
    
    We are no longer using flask
    Build and Test needs to change to reflect this, master meta-package definitions changed
    
    * update testing docker file to reflect changes to system dependencies
    
    * Removed anonymous namespaces;
    Merged `.gitignore`s;
    Added `rclcpp::ok()` to main loop condition;
    
    ---------
    
    Co-authored-by: Zac Stanton <zac71113@gmail.com>
    Co-authored-by: catfishjw <james.wiley@live.com>

---
+++ src/driverstation/driverstation_gui/include/driverstation_gui/joystick_ui.hpp
@@ -0,0 +1,63 @@
+#ifndef JOYSTICK_UI_HEADER_INCLUDED
+#define JOYSTICK_UI_HEADER_INCLUDED
+
+#include <assert.h>
+#include <memory>
+#include <raylib.h>
+#undef RAYGUI_IMPLEMENTATION
+#include <raygui.h>
+#include <vector>
+
+#include "vec_rec_util.hpp"
+
+namespace driverstation::gui{
+	struct JoystickDisplay{
+		int joystick_id;
+		std::vector<Rectangle> button_indicators;
+		std::vector<Rectangle> axis_indicators;
+		std::vector<const char*> axis_labels;
+
+		// TODO: Should these kinds of things take floats as arguments instead?
+		JoystickDisplay(int joystick_id, std::vector<Rectangle> button_indicators, std::vector<Rectangle> axis_indicators, std::vector<const char*> axis_labels){
+			assert(axis_indicators.size() == axis_labels.size());
+
+			this->joystick_id = joystick_id;
+			this->button_indicators = button_indicators;
+			this->axis_indicators = axis_indicators;
+			this->axis_labels = axis_labels;
+		}
+
+		// TODO: Should JoystickDisplay use a RenderTexture like other components?
+		// TODO: parameter `color` is currently unused
+		void draw(int x, int y, [[maybe_unused]] Color color){
+			bool joystick_available = IsGamepadAvailable(this->joystick_id);
+
+			int previous_gui_state = GuiGetState();
+			if(joystick_available) GuiEnable();
+			else GuiDisable();
+
+			for(size_t i = 0; i < this->button_indicators.size(); i++){
+				bool button_state = IsGamepadButtonDown(this->joystick_id, i + 1);
+				GuiCheckBox(
+					this->button_indicators[i] + Vector2{(float)x, (float)y},
+					TextFormat("%d", i + 1),
+					&button_state
+				);
+			}
+			for(size_t i = 0; i < this->axis_indicators.size(); i++){
+				// NOTE: Buttons are 1-indexed, Axes are 0-indexed
+				float axis_value = GetGamepadAxisMovement(this->joystick_id, i) + 1.0f;
+				GuiProgressBar(
+					this->axis_indicators[i] + Vector2{(float)x, (float)y},
+					nullptr, TextFormat(this->axis_labels[i], axis_value),
+					&axis_value, 0.0f, 2.0f  // NOTE: Progress bars don't seem to like negative values
+				);
+			}
+
+			GuiSetState(previous_gui_state);
+		}
+		void drawV(Vector2 position, Color color){this->draw((int)position.x, (int)position.y, color);}
+	};
+}
+
+#endif
