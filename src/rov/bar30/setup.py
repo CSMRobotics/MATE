bd1c7490e639b27aa038dd0b04d0641cc9a81f20
catfishjw <james.wiley@live.com>
2024-02-11 14:39:32 -0700

    replace all references to bar02 with bar30

--- src/rov/bar30/setup.py
+++ src/rov/bar30/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'bar02'
+package_name = 'bar30'
 
 setup(
     name=package_name,
@@ -15,12 +15,12 @@ setup(
     zip_safe=True,
     maintainer='Zac Stanton',
     maintainer_email='zac71113@gmail.com',
-    description='ROS 2 package for MS5837-02BA Blue Robotics Depth Sensor',
+    description='ROS 2 package for MS5837-30BA Blue Robotics Depth Sensor',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'bar02 = bar02.bar02:main'
+            'bar30 = bar30.bar30:main'
         ],
     },
 )
