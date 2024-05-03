671490789ce91dabeb360bb36156c4877952b2eb
James Wiley <james.wiley@live.com>
2024-05-03 17:27:00 -0600

    add ui handshake

--- src/rov/LEDController/src/rov/LEDCOntroller/LEDController.py
+++ src/rov/LEDController/src/rov/LEDCOntroller/LEDController.py
@@ -1,4 +1,5 @@
 from common.csm_common_interfaces.msg import PinState
+from std_msgs.msg import String
 import Jetson.GPIO as GPIO
 import rclpy
 from rclpy.node import Node
@@ -9,22 +10,38 @@ import neopixel
 LED_PIN = 0
 BLACK = (0,0,0)
 WHITE = (255,255,255)
-PIXELS = neopixel.NeoPixel(LED_PIN, 100)
+NUM_LIGHTS = 100
+PIXELS = neopixel.NeoPixel(LED_PIN, NUM_LIGHTS)
 
-class RovGpio(Node):
+class LEDControllerNode(Node):
     
     def __init__(self):
         super().__init__(node_name="LEDController")
         self.state_subscriber = self.create_subscription(PinState, "set_rov_gpio", self.on_set_rov_gpio, 10)
         self.rgb_subscriber = self.create_subscription(RGBState, "LED_strip_state", self.on_set_rgb, 10)
 
+        self.publisher_ = self.create_publisher(String, 'preprogrammed_animations', 10)
+        
+        self.animations = [
+            "animation1",
+            "animation2"
+        ]
+        
+        # Listen for requests from the UI
+        self.ui_subscriber = self.create_subscription(String, 'ui_requests', self.ui_request_callback, 10)
+
     
         GPIO.setmode(GPIO.BOARD)
-        
         GPIO.setup(LED_PIN, GPIO.OUT)
         PIXELS.fill(BLACK)
 
-    
+    def ui_request_callback(self, msg):
+        if msg.data == 'get_animations':
+            # Send the list of preprogrammed animations to the UI
+            animations_msg = String()
+            animations_msg.data = "\n".join(animation for animation in self.animations if animation.startswith('custom_'))
+            self.publisher_.publish(animations_msg)
+            self.get_logger().info('Sent preprogrammed animations to UI')
     
     def on_set_rov_gpio(self, message: PinState):
             if message.pin not in LED_PIN:
@@ -34,5 +51,14 @@ class RovGpio(Node):
 
     def LED_strip_state(self, message: RGBState):
          pass
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    led_controller = LEDControllerNode()
+    rclpy.spin(led_controller)
+    led_controller.destroy_node()
+    rclpy.shutdown()
+    
 if __name__ == '__main__':
     main()
