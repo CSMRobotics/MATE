34ea1ddc5c6bb99f340932e3e0aa8855be038fd6
James Wiley <james.wiley@live.com>
2024-05-03 17:17:34 -0600

    low level led control, no animations yet

--- src/rov/LEDController/src/rov/LEDCOntroller/LEDController.py
+++ src/rov/LEDController/src/rov/LEDCOntroller/LEDController.py
@@ -7,28 +7,30 @@ import board
 import neopixel
 
 LED_PIN = 0
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+PIXELS = neopixel.NeoPixel(LED_PIN, 100)
 
 class RovGpio(Node):
+    
     def __init__(self):
         super().__init__(node_name="LEDController")
         self.state_subscriber = self.create_subscription(PinState, "set_rov_gpio", self.on_set_rov_gpio, 10)
         self.rgb_subscriber = self.create_subscription(RGBState, "LED_strip_state", self.on_set_rgb, 10)
 
     
-    GPIO.setmode(GPIO.BOARD)
-    pixels = neopixel.NeoPixel(LED_PIN, 100)
+        GPIO.setmode(GPIO.BOARD)
+        
+        GPIO.setup(LED_PIN, GPIO.OUT)
+        PIXELS.fill(BLACK)
 
-    GPIO.setup(LED_PIN, GPIO.OUT)
+    
     
     def on_set_rov_gpio(self, message: PinState):
             if message.pin not in LED_PIN:
                 pass
-
-            if message.pin not in self.output_pins:
-                GPIO.setup(message.pin, GPIO.OUT)
-                self.output_pins.add(message.pin)
-
-            GPIO.output(message.pin, GPIO.HIGH if message.state else GPIO.LOW)
+            
+            PIXELS.fill(WHITE)
 
     def LED_strip_state(self, message: RGBState):
          pass
