b0b4795faf40f5b156d1205210f77d5f65f0d8bf
Zac Stanton <zac71113@gmail.com>
2024-05-03 14:48:23 -0600

    Update rov_cameras_launch.py
    TODO:
    nvarguscamerasrc should have sensor-id=<id> to allow for two mipi cameras

--- src/rov/rov_cameras/launch/rov_cameras_launch.py
+++ src/rov/rov_cameras/launch/rov_cameras_launch.py
@@ -4,5 +4,11 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(node_name="rov_cameras",
-             executable="rov_cameras")
+             executable="rov_cameras",
+        ExecuteProcess(
+            #cmd=["ros2", "run", "camera_stream", "rtsp", "\"videotestsrc ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96\""], # This is a test pipeline for non-nvarguscamerasrc compatible systems
+            cmd=["ros2", "run", "camera_stream", "rtsp", "\"nvarguscamerasrc ! video/x-raw(memory:NVMM),width=1920,height=1080 ! nvvidconv ! nvv4l2h264enc ! h264parse ! rtph264pay name=pay0 pt=96\""],
+            output='both',
+            shell=True
+        )
     ])
