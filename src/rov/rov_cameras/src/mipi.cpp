9d3841d9bd21acf7af6d72af85fb611eca6e0731
Zac Stanton <zac71113@gmail.com>
2024-05-03 14:40:12 -0600

    Create mipi.cpp

---
+++ src/rov/rov_cameras/src/mipi.cpp
@@ -0,0 +1,76 @@
+extern "C" {
+#include <gst/gst.h>
+#include <gst/rtsp-server/rtsp-server.h>
+#include <stdio.h>
+}
+
+#include <string>
+
+#define DEFAULT_RTSP_PORT "8554"
+
+static char *port = (char *) DEFAULT_RTSP_PORT;
+
+static GOptionEntry entries[] = {
+  {"port", 'p', 0, G_OPTION_ARG_STRING, &port,
+      "Port to listen on (default: " DEFAULT_RTSP_PORT ")", "PORT"},
+  {NULL}
+};
+
+int main (int argc, char *argv[]) {
+  GMainLoop *loop;
+  GstRTSPServer *server;
+  GstRTSPMountPoints *mounts;
+  GstRTSPMediaFactory *factory;
+  GOptionContext *optctx;
+  GError *error = NULL;
+
+  optctx = g_option_context_new ("<launch line> - Test RTSP Server, Launch\n\n"
+      "Example: \"( videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 )\"");
+  g_option_context_add_main_entries (optctx, entries, NULL);
+  g_option_context_add_group (optctx, gst_init_get_option_group ());
+  if (!g_option_context_parse (optctx, &argc, &argv, &error)) {
+    g_printerr ("Error parsing options: %s\n", error->message);
+    g_option_context_free (optctx);
+    g_clear_error (&error);
+    return -1;
+  }
+  g_option_context_free (optctx);
+
+  loop = g_main_loop_new (NULL, FALSE);
+
+  /* create a server instance */
+  server = gst_rtsp_server_new ();
+  g_object_set (server, "service", port, NULL);
+
+  /* get the mount points for this server, every server has a default object
+   * that be used to map uri mount points to media factories */
+  mounts = gst_rtsp_server_get_mount_points (server);
+
+  /* make a media factory for a stream. The default media factory can use
+   * gst-launch syntax to create pipelines.
+   * any launch line works as long as it contains elements named pay%d. Each
+   * element with pay%d names will be a stream */
+  factory = gst_rtsp_media_factory_new ();
+  gst_rtsp_media_factory_set_launch (factory, argv[1]);
+  gst_rtsp_media_factory_set_shared (factory, TRUE);
+
+  /* Check if stream ID is valid */
+  if(argv[2] != "0" || argv[2] != "1") {
+    fprintf(stderr, "Expected stream ID 0 or 1 got %s instead", argv[2]);
+    exit(1);
+  }
+  /* attach the factory to the /rov url */
+  gst_rtsp_mount_points_add_factory (mounts, "/rov" + argv[2], factory);
+
+  /* don't need the ref to the mapper anymore */
+  g_object_unref (mounts);
+
+  /* attach the server to the default maincontext */
+  gst_rtsp_server_attach (server, NULL);
+
+  /* start serving */
+  g_print ("stream ready at rtsp://127.0.0.1:%s/rov\n", port);
+  g_main_loop_run (loop);
+
+  return 0;
+}
