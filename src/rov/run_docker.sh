a20fc0c99e1640c9d8ecc1b97eca847133125bd3
yameatmeyourdead <zac71113@gmail.com>
2024-05-04 00:44:23 +0000

    Fix xauth

--- src/rov/run_docker.sh
+++ src/rov/run_docker.sh
@@ -5,7 +5,9 @@ docker run --runtime nvidia -it --rm \
     --env GID=$(id -g) \
     --pid=host \
     --ipc=host \
+    --net=host \
     --gpus all \
+    --privileged \
     -v /dev/shm:/dev/shm \
     -v /bin/systemctl:/bin/systemctl \
     -v /run/systemd/system:/run/systemd/system \
@@ -15,8 +17,11 @@ docker run --runtime nvidia -it --rm \
     -v /dev:/dev \
     -v /run/udev:/run/udev:ro \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ~/.Xauthority:/root/.Xauthority:rw \
+    -v /tmp/argus_socket:/tmp/argus_socket \
+    --cap-add SYS_PTRACE \
     -v /usr/lib/aarch64-linux-gnu/gstreamer-1.0/libgstnvcompositor.so:/usr/lib/aarch64-linux-gnu/gstreamer-1.0/libgstnvcompositor.so \
     -e DISPLAY=$DISPLAY \
     -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
-    csmmaterobotics/competition:latest
+    csmmaterobotics/rov:latest
 # docker run -it --rm --runtime nvidia -v /tmp/.X11-unix/:/tmp/.X11-unix csmmaterobotics/rov:latest
