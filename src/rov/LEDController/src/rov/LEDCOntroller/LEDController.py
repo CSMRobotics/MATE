6fdbfd7d0b4444bbee8d1ab9273b201d9a97225c
James Wiley <james.wiley@live.com>
2024-05-03 18:57:20 -0600

    add LED colors

--- src/rov/LEDController/src/rov/LEDCOntroller/LEDController.py
+++ src/rov/LEDController/src/rov/LEDCOntroller/LEDController.py
@@ -10,10 +10,10 @@ import neopixel
 import time
 import random
 
+NUM_LIGHTS = 60
 LED_PIN = 0
 BLACK = (0,0,0)
 WHITE = (255,255,255)
-NUM_LIGHTS = 100
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
 GREEN = (0, 255, 0)
@@ -22,13 +22,13 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 PIXELS = neopixel.NeoPixel(LED_PIN, NUM_LIGHTS)
 global ledState
+global ledColor
 
 class LEDControllerNode(Node):
     
     def __init__(self):
         super().__init__(node_name="LEDController")
         self.state_subscriber = self.create_subscription(PinState, "set_rov_gpio", self.on_set_rov_gpio, 10)
-        self.rgb_subscriber = self.create_subscription(RGBState, "LED_strip_state", self.on_set_rgb, 10)
         self.publisher_ = self.create_publisher(String, 'preprogrammed_animations', 10)
         
         self.animations = [
@@ -37,6 +37,17 @@ class LEDControllerNode(Node):
             "pulse",
             "solid",
             "seizure_disco"
+        ]
+
+        self.colors = [
+            "WHITE",
+            "BLACK",
+            "RED",
+            "YELLOW",
+            "GREEN",
+            "CYAN",
+            "BLUE",
+            "PURPLE"
         ]
         
         # Listen for requests from the UI
@@ -50,41 +61,43 @@ class LEDControllerNode(Node):
         if msg.data == 'get_animations':
             # Send the list of preprogrammed animations to the UI
             animations_msg = String()
-            animations_msg.data = "\n".join(animation for animation in self.animations if animation.startswith('custom_'))
+            animations_msg.data = "\n".join(animation for animation in self.animations)
             self.publisher_.publish(animations_msg)
             self.get_logger().info('Sent preprogrammed animations to UI')
+        elif msg.data == 'get_colors':
+            colors_msg = String()
+            colors_msg.data = "\n".join(color for color in self.colors)
+            self.publisher_.publish(colors_msg)
+            self.get_logger().info('Sent preprogrammed colors to UI')
         else:
             # Check if the requested animation exists
             if msg.data in self.animations:
                 # Call the method corresponding to the requested animation
                 
                 if ledState and (lastAnimation != msg.data):
-                    getattr(self, lastAnimation)()
+                    exec(f"self.{msg.data}({ledColor}, 0.1)")()
                 else:
                     PIXELS.brightness(0)
-            lastAnimation = msg.data
+                lastAnimation = msg.data
+            elif msg.data in self.colors:
+                lastColor = ledColor
+                if ledState and (lastColor != msg.data):
+                    exec(f"self.{lastAnimation}({msg.data}, 0.1)")()
+
 
     def on_set_rov_gpio(self, message: PinState):
         if message.pin not in LED_PIN:
             pass
         ledState = message.state
-
-def main(args=None):
-    rclpy.init(args=args)
-    led_controller = LEDControllerNode()
-    rclpy.spin(led_controller)
-    led_controller.destroy_node()
-    rclpy.shutdown()
     
-    
-    def color_chase(color, wait):
+    def color_chase(color, wait=0.1):
         for i in range(NUM_LIGHTS):
             PIXELS[i] = color
             time.sleep(wait)
             PIXELS.show()
         time.sleep(0.5)
 
-    def rainbow_cycle(wait):
+    def rainbow_cycle(color, wait=0.1):
         for j in range(255):
             for i in range(NUM_LIGHTS):
                 rc_index = (i * 256 // NUM_LIGHTS) + j
@@ -92,7 +105,7 @@ def main(args=None):
             PIXELS.show()
             time.sleep(wait)
 
-    def pulse(color, wait):
+    def pulse(color, wait=0.1):
         PIXELS.fill(color)
         for i in range(255):
             PIXELS.setBrightness(i)
@@ -100,11 +113,11 @@ def main(args=None):
             PIXELS.show()
         time.sleep(wait)
 
-    def solid(color):
+    def solid(color, wait=0.1):
         PIXELS.fill(color)
         PIXELS.show()
 
-    def seizure_disco(wait):
+    def seizure_disco(color, wait=0.1):
         for i in range(NUM_LIGHTS):
             R = random.randint(0,255)
             G = random.randint(0,255)
